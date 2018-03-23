4a558e3c44adb4e7a314326fae1dddd0aaded5b5
sdash <sdash@ncgr.org>
2018-03-22 19:31:47 -0500

    for workshop

--- theme/templates/lis_expression_demo_workshop.tpl.php
+++ theme/templates/lis_expression_demo_workshop.tpl.php
@@ -13,3 +13,35 @@
 ?>
 
 <h2>Expression Data At LIS</h2>
+<a target="_blank"  href="https://legumeinfo.org">https://legumeinfo.org</a>
+
+<h3>How to reach the expression data:</h3>
+<u>From Gene Search: </u><br>
+At home page: Gene Search button (2nd row) --> Select a species (cajca, cicar.ICC4958, phavu, vigun) & Search  --> select a gene from the result table --> Click  'Expression' on the left panel. And, explore the expression data for this gene.<br>
+OR<br>
+straight go to <a href="/search/gene?abbreviation=phavu">'Gene Search' page</a> for common bean and click the 2nd gene, 'phavu.Phvul.001G000200'.
+<br>
+<br>
+<u>From Expression Summary page:</u><br>
+At home page: Expression button (4th row) --> Click one of the 4 datasets available for now. <br>
+OR <br>
+straight to <a href="/lis_expression/all">Expression Summary Page</a>
+
+
+<h3>Examples to explore</h3>
+
+<ol>
+  <li>A photosynthetic gene expressed only in leaves.</li>
+  <li>The family members have similar expression profiles.</li>
+  <li>The family members do not follow similar expression profiles.</li>
+  <li>The hooded cobra profile</li>
+  <li></li>
+  <li>Finally send the list of profile neighbors to the LegumeMine for further analysis</li>
+</ol>
+
+<!--
+Telling you how to view gene expression is plain boring:
+Now time for my vow of silence and time for you to explore and give me some feedback. Any feedback is useful.
+-->
+
+
